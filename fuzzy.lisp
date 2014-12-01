d7eeeed0461bdde9620ac45c0588a1a1dea406c3
Daniel Guimaraes <dcguim@Daniels-MacBook-Pro.local>
2014-12-01 13:14:58 -0200

    added the normalized function to verify if a given set is regular fuzzy set and the height function

--- fuzzy.lisp
+++ fuzzy.lisp
@@ -31,7 +31,38 @@
 	   (dolist (e a (reverse new))
 	     (setq rev (- 1 (cadr e)))
 	     (setq new (cons (list (car e) rev) new))))))
-		   	     
+
+(defun fuzzy-height(a)
+  (cond ((null a) 0)
+	(t (setq h 0)
+	   (dolist (e a val)
+	     (setq val (cadr e))
+	     (if (> val h)
+		 (setq h val)))
+	   (return-from fuzzy-height h))))
+
+(defun normalized(a)
+  (cond ((null a) t)
+	(t (setq norm-h nil)
+	   (setq neg-num nil)
+	   (if (equal (fuzzy-height a) 1) 
+	       (setq norm-h t))
+	   (dolist (e a)
+	     (if (< (cadr e) 0)
+		 (setq neg-num t))
+	     (if (and (equal neg-num nil) (equal norm-h t))
+		 t nil))))
+  
+
+	       
+	       
+	      
+	 
+
+	   
+	     
+	
+	     
 	   
 
 		   
