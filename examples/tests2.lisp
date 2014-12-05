486d8e3274ed856d0be302e2934978ecaa6ebe49
Daniel Guimaraes <dcguim@gmail.com>
2014-12-05 15:36:13 -0200

    implemented modifier not andcreated simple examples for the modifiers

---
+++ examples/tests2.lisp
@@ -0,0 +1,16 @@
+;;test simple modifiers
+;;; (not (slightly A))
+(let ((a '((0 1/9) (1 2/5) (2 7/16) (3 15/17))))
+	      (no (slightly a)))
+
+;;;(extremely (not A))
+(let ((a '((0 1/9) (1 2/5) (2 7/16) (3 15/17))))
+	      (extremely (no a)))
+
+;;; (little (not A))
+ (let ((a '((0 1/9) (1 2/5) (2 7/16) (3 15/17))))
+	      (little (no a)))
+
+;;;(extremely (little A))
+(let ((a '((0 1/9) (1 2/5) (2 7/16) (3 15/17))))
+	      (extremely (little a)))
--- modifiers.lisp
+++ modifiers.lisp
@@ -23,3 +23,18 @@
       (let ((fst (car a)))
 	 (cons `(,(car fst) ,(expt (cadr fst) (/ 1 3)))
 	       (slightly (cdr a))))))
+
+;; the no modifier is exactly like the fuzzy-comp 
+;;operator but implemented differently
+(defun build-not-list (a)
+  (if (null a) nil
+      (let ((fst (car a)))
+	(cons `(,(car fst) ,(- 1 (cadr fst))) 
+	      (build-not-list (cdr a))))))
+(defun no (a)
+  (if (null a)
+      (if (equal *universe* nil)
+	  (error "First is necessary to define the universe of discourse with the def-universe function") *universe*)
+      (build-not-list a)))
+
+
